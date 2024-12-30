8f611f26acd2424f676afcf390694ee269f6e233
FatmaShagar <fatmashagar64@gmail.com>
2024-12-30 16:25:35 +0200

    some change in calling api

--- lib/features/auth/presentation/view/widgets/socail_buttons.dart
+++ lib/features/auth/presentation/view/widgets/socail_buttons.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-// import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import '../../../../../core/localization/app_localization.dart';
 import '../../../../../core/utils/image_paths.dart';
 import 'social_button.dart';
@@ -9,37 +9,37 @@ import 'social_button.dart';
 class SocialButtons extends StatelessWidget {
   SocialButtons({super.key});
 
-  // final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  // Future<void> _signInWithGoogle(BuildContext context) async {
-  //   try {
-  //     final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-  //
-  //     if (googleUser != null) {
-  //       final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-  //       Navigator.pushReplacementNamed(context, 'Home Screen');
-  //     }
-  //   } catch (error) {
-  //     print("Error during Google Sign-In: $error");
-  //   }
-  // }
-  // Future<void> _signInWithApple(BuildContext context) async {
-  //   try {
-  //     final AuthorizationCredentialAppleID credential = await SignInWithApple.getAppleIDCredential(
-  //       scopes: [
-  //         AppleIDAuthorizationScopes.email,
-  //         AppleIDAuthorizationScopes.fullName,
-  //       ],
-  //     );
-  // print('Apple Sign-In Success');
-  //     print('Apple ID Token: ${credential.identityToken}');
-  //
-  //
-  //     Navigator.pushReplacementNamed(context, 'Home SCreen');
-  //   } catch (error) {
-  //     print("Error during Apple Sign-In: $error");
-  //   }
-  // }
+  Future<void> _signInWithGoogle(BuildContext context) async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+      if (googleUser != null) {
+        final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+        Navigator.pushReplacementNamed(context, 'Home Screen');
+      }
+    } catch (error) {
+      print("Error during Google Sign-In: $error");
+    }
+  }
+  Future<void> _signInWithApple(BuildContext context) async {
+    try {
+      final AuthorizationCredentialAppleID credential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+  print('Apple Sign-In Success');
+      print('Apple ID Token: ${credential.identityToken}');
+
+
+      Navigator.pushReplacementNamed(context, 'Home SCreen');
+    } catch (error) {
+      print("Error during Apple Sign-In: $error");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
